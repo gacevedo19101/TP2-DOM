33edd670ca5516e0f2f6fa541ed2f1d49724350f
Acevedo Gonzalo <acevedo.gonzalo@frlr.utn.edu.ar>
2025-05-22 23:54:25 -0300

    Actualizo mi script con un nuevo confirm integrado

--- fusiones-con-conflictos/js/script.js
+++ fusiones-con-conflictos/js/script.js
@@ -1,7 +1,3 @@
 console.log("¡Bienvenido a mi página!")
 console.log("Este es un scrit que valida que funciona la página")
-alert("Bienvenido")
-
-const date = ''
-const mensaje = prompt("Esta listo para esta nueva experiencia en la programación?")
-confirm("Confirma que accede a la informacion?")
+const bool = confirm("¿Es tu primera vez en el sitio?")
