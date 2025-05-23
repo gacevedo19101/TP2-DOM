65d52903bfff4b123fba041311c1c2e090ba3a86
Acevedo Gonzalo <acevedo.gonzalo@frlr.utn.edu.ar>
2025-05-22 23:56:03 -0300

    Incorporo un condicional para mostrar dos msje segun la visita

--- fusiones-con-conflictos/js/script.js
+++ fusiones-con-conflictos/js/script.js
@@ -1,3 +1,8 @@
 console.log("¡Bienvenido a mi página!")
 console.log("Este es un scrit que valida que funciona la página")
 const bool = confirm("¿Es tu primera vez en el sitio?")
+if(bool){
+    alert("Bienvenido por primera vez a nuestro sitio")
+} else{
+    alert("¡Hola tu de nuevo!")
+}
