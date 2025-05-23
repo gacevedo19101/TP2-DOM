cc68014e60e958a137a46272a2da471ead2aab61
Acevedo Gonzalo <acevedo.gonzalo@frlr.utn.edu.ar>
2025-05-22 23:57:12 -0300

    Se retira lo que no se usa en el script e incorporo un ingreso de nombre

--- fusiones-con-conflictos/js/script.js
+++ fusiones-con-conflictos/js/script.js
@@ -2,6 +2,4 @@ console.log("¡Bienvenido a mi página!")
 console.log("Este es un scrit que valida que funciona la página")
 alert("Bienvenido")
 
-const date = ''
-const mensaje = prompt("Esta listo para esta nueva experiencia en la programación?")
-confirm("Confirma que accede a la informacion?")
+const mensaje = prompt("Ingresa tu nombre:")
