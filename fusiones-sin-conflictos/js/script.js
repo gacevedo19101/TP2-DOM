0622c55933b4555ab1da5958d1629d3bbc4420d4
Acevedo Gonzalo <acevedo.gonzalo@frlr.utn.edu.ar>
2025-05-22 23:19:53 -0300

    Actualizo el script con un confirm

--- fusiones-sin-conflictos/js/script.js
+++ fusiones-sin-conflictos/js/script.js
@@ -1,3 +1,7 @@
 console.log("¡Bienvenido a mi página!")
 console.log("Este es un scrit que valida que funciona la página")
 alert("Bienvenido")
+
+const date = ''
+const mensaje = prompt("Esta listo para esta nueva experiencia en la programación?")
+confirm("Confirma que accede a la informacion?")
